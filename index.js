9f5c47beb2e9139d5490b76ab04dbe72efcdcd16
katiecnelson <78412159+katiecnelson@users.noreply.github.com>
2021-05-16 12:34:36 +0100

    Added error checking and updated to use jquery

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-var button = document.querySelector("button");
-var inputBox1 = document.querySelector(".input1");
-var inputBox2 = document.querySelector(".input2");
-var inputBox3 = document.querySelector(".input3");
-var inputBox4 = document.querySelector(".input4");
-var inputBox5 = document.querySelector(".input5");
-var inputBox6 = document.querySelector(".input6");
-var inputBox7 = document.querySelector(".input7");
-var inputBox8 = document.querySelector(".input8");
-var inputBox9 = document.querySelector(".input9");
-var percentText = document.querySelector("#percent");
-var resultText = document.querySelector("#result");
 var percent = 0;
 var result = "";
-var numbers = [];
+var results = [];
 
-button.addEventListener("click", function() {
+$(".btn").click(function() {
+  $(".input").each(function() {
+    results.push(parseFloat(this.value));
+    console.log(results);
+  });
 
-  calculatePercentage();
-  determineResult();
+  if (results.includes(NaN)) {
+    alert("Please enter a percentage number for each module!");
+    location.reload();
 
-  resultText.innerHTML = result;
-  percentText.innerHTML = percent.toFixed(2) + "%";
-
+  } else {
+    calculatePercentage();
+    determineResult();
+    displayResults();
+    results = [];
+  }
 });
 
-
 function calculatePercentage() {
-  input1 = parseFloat(inputBox1.value);
-  input2 = parseFloat(inputBox2.value);
-  input3 = parseFloat(inputBox3.value);
-  input4 = parseFloat(inputBox4.value);
-  input5 = parseFloat(inputBox5.value);
-  input6 = parseFloat(inputBox6.value);
-  input7 = parseFloat(inputBox7.value);
-  input8 = parseFloat(inputBox8.value);
-  input9 = parseFloat(inputBox9.value);
-  var tenCreditTotal = input1 + input2 + input5 + input6;
-  var twentyCreditTotal = (input3 + input4 + input7 + input8) * 2;
-  var projectTotal = input9 * 6;
+  var tenCreditTotal = results[0] + results[1] + results[4] + results[5];
+  var twentyCreditTotal = (results[2] + results[3] + results[6] + results[7]) * 2;
+  var projectTotal = results[8] * 6;
   percent = (tenCreditTotal + twentyCreditTotal + projectTotal) / 18;
 }
 
@@ -51,4 +37,9 @@ function determineResult() {
   } else {
     result = "Fail";
   }
+}
+
+function displayResults() {
+  $("#result").html(result);
+  $("#percent").html(percent.toFixed(2) + "%");
 }
